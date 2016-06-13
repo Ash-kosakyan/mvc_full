e163327a4400253a82caa4664255071682986149
Ashot <Ash.kosakyan@gmail.com>
2016-06-13 20:11:27 +0400

    STEP 2 - Late Static Bindings + Abstract Class

--- models/News.php
+++ models/News.php
@@ -1,36 +1,25 @@
 <?php
 
 require_once __DIR__ . '/../classes/DB.php';
+require_once __DIR__ . '/AbstractModel.php';
 
-class News {
+class News extends AbstractModel{
 
     public $id;
     public $title;
     public $content;
     public $author;
-    
+
+    protected static $table = 'posts';
+    protected static $class = 'News';
+
     public function __set($name, $value)
     {
         
     }
-
     public function __get($name)
     {
         
-    }
-
-    public static function getAll()
-    {
-        $db = new DB();
-        $news = $db->queryAll("SELECT * FROM posts", 'News');
-        return $news;
-    }
-    
-    public static function getOne($id)
-    {
-        $db = new DB();
-        $new = $db->queryOne("SELECT * FROM posts WHERE id=" . $id, 'News');
-        return $new;
     }
 }
 
